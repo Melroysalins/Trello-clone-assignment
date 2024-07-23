02e8140971be1a4590d9183755c1abb76c81a15c
Melroysalins <melroysalins@gmail.com>
2024-07-23 09:06:02 +0530

    Navigation Bar frontend design

---
+++ src/components/navigationBar/index.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./index.css";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import LoginMessage from "../login";
+
+const NavigationBar = () => {
+  const [selectedinfo, setSelectedInfo] = useState("");
+  return (
+    <div className="NavigationBar">
+      <AssignmentIcon className="assignmentIcon" />
+      <div className="loginSignupContainner">
+        <LoginMessage
+          message={"Login"}
+          selectedinfo={selectedinfo}
+          setSelectedInfo={setSelectedInfo}
+        />
+        <LoginMessage
+          message={"Signup"}
+          selectedinfo={selectedinfo}
+          setSelectedInfo={setSelectedInfo}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./index.css";
+import NavigationBar from "../../components/navigationBar";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="HomePage">
+      <NavigationBar />
+    </div>
+  );
 };
 
 export default HomePage;
